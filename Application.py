791413879b2bf6e1f88a9675d48c043a1cbc8059
wahyudesu <96912274+wahyudesu@users.noreply.github.com>
2024-03-21 16:34:29 +0700

    update

--- Application.py
+++ Application.py
@@ -1,158 +1,45 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
 from Game import Analyzer
-
 import torch
 import torch.nn.functional as f
-
 from transformers import BertModel, BertTokenizer
-
 from time import sleep
-
 import os
-
-
 
 #forms functions
 
+nama = st.text_input("Masukkan nama Anda:")
+
 def formCreation():
-    
-
-    st.header('Contexto')
-
-    languages= ( 'English', 'русский', 'العربية')
-   
+    st.header('Gemastik Pengembangan Aplikasi Permainan')
+    #languages= ( 'English', 'русский', 'العربية')
+    languages= ( 'Indonesia', 'English')
     chooseform(languages)
-
     return
-
-
-
 
 def chooseform(languages):
     languageOption = st.selectbox(
         'please select your language',
         languages)
     
-    if (languageOption== 'العربية'):
-        directory = os.getcwd()
-        guessesPath= directory + "\\guesses.txt"
-        ArabicContexto = createArabicEnviroment()
-        ArabicForm(guessesPath,ArabicContexto)
-        return 
-    
-    
-    elif (languageOption== 'English'):
+    if (languageOption== 'English'):
         directory = os.getcwd()
         guessesPath= directory + "\\guesses.txt"
         englishContexto= createEnglishEnvironment()
         EnglishForm(guessesPath, englishContexto)
         return
     else:
-
-        directory = os.getcwd()
-        guessesPath= directory + "\\guesses.txt"
-        RussianContexto = createRussianEnviroment()
-        RussianForm(guessesPath,RussianContexto)
-        return
+        # Tidak ada opsi lain selain English, jadi kita tidak perlu menangani kasus lain
+        pass
 
 #Forms
-def ArabicForm(guessesPath,ArabicContexto):
-    Arabiccontainer = st.container()
-    guess= Arabiccontainer.text_input("رجاء: أدخل كلمة للتخمين", "type a word")
-    guess= guess.strip()
-    
-    if guess == "type a word":
-        Arabiccontainer.info(""" **كيفية اللعب:**
-        لديك عدد غير محدود من التخمينات لتتمكن من العثور على الكلمة السرية:
-        تم ترتيب الكلمات عن طريق خوارزمية ذكاء صنعي وفقاً لمدى التقارب بين الكلمة السرية والتخمينات.
-        بعد اختيارك للتخمين سترى مدى قرب التخمين من الكلمة السرية، حيث تمتلك الكلمة السرية القيمة 1 وتقل هذه القيمة كلما كان التخمين بعيد عن الكلمة السرية.""")
-
-        f = open(guessesPath, "w")
-        f.write("Words , Similarities \n")
-        f.close() 
-    
-    else:
-        
-        arabicContextoDone=False
-        _ ,arabicContextoSim, arabicContextoDone, new_target_arabic= checkSimilarity(guess, ArabicContexto)         
-        guessWrite= guess+  " , " + str(arabicContextoSim*1)      
-
-    
-        if not arabicContextoDone:
-            if arabicContextoSim== (-1000):
-                st.write(" الكلمة "+ guess+" غير موجودة")
-                show(arabicContextoDone, guessesPath, guessWrite=None)
-            else:
-                show(arabicContextoDone, guessesPath, guessWrite)
-            if st.button('Give Up'):
-                st.write(ArabicContexto.giveup())            
-        else:
-            st.write("**مبارك: لقد استطعت تخمين الكلمة الصحيحة**")
-            st.balloons()
-            setNewTarget("Arabic", new_target_arabic)
-            show(arabicContextoDone, guessesPath, guessWrite)
-            f = open(guessesPath, "w")
-            f.write("Words , Similarities \n")
-            f.close() 
-    
-    return 
-
-def RussianForm(guessesPath,RussianContexto):
-    
-    Russiancontainer = st.container()
-    guess= Russiancontainer.text_input("Пожалуйста, введите свое предположение:", "type a word")
-    guess= guess.strip()
-
-    if guess == "type a word":
-        Russiancontainer.info(""" **как играть:**  
-        Найди секретное слово. У вас есть неограниченное количество догадок.  
-        Слова были отсортированы с помощью алгоритма искусственного интеллекта в соответствии с тем, насколько они были похожи на секретное слово.  
-        После отправки слова вы увидите его позицию. Секретное слово - это цифра 1.  
-        Алгоритм проанализировал тысячи текстов. Он использует контекст, в котором используются слова, для вычисления сходства между ними.""")
-
-        f = open(guessesPath, "w")
-        f.write("Words , Similarities \n")
-        f.close() 
-        f = open(guessesPath, "w")
-        f.write("Words , Similarities \n")
-        f.close() 
-    
-    else:
-
-        russianContextoDone=False
-        _ ,russianContextoSim, russianContextoDone, new_target_Russian= checkSimilarity(guess, RussianContexto) 
-        guessWrite= guess+  " , " + str(russianContextoSim*1)
-    
-        if not russianContextoDone:
-            if russianContextoSim== (-1000):
-                st.write("Это слово "+ guess+" не существует")
-                show(russianContextoDone, guessesPath, guessWrite=None)
-            else:
-                show(russianContextoDone, guessesPath, guessWrite)
-            if st.button('Give Up'):
-                st.write(RussianContexto.giveup())            
-        else:
-            st.write("**Поздравляем, вы угадали секретное слово**")
-            st.balloons()
-            setNewTarget("Russian", new_target_Russian)
-            show(russianContextoDone, guessesPath, guessWrite)
-            f = open(guessesPath, "w")
-            f.write("Words , Similarities \n")
-            f.close() 
-    
-    return
-
-
 def EnglishForm(guessesPath, englishContexto):
     Englishcontainer = st.container()
 
     guess= Englishcontainer.text_input("Please enter your guess:", "type a word")
     guess= guess.strip()
-    #guess= "agricultural"
-    
 
     if guess == "type a word":
         Englishcontainer.info("""**how to play:**  
@@ -169,7 +56,7 @@ def EnglishForm(guessesPath, englishContexto):
         englishContextoDone=False
         _ ,englishContextoSimilarity, englishContextoDone, new_target_English= checkSimilarity(guess, englishContexto)
         if type(englishContextoSimilarity) != int:
-            guessWrite= guess+  " , " + str(englishContextoSimilarity*1)
+            guessWrite= guess+ " , " + str(englishContextoSimilarity*1)
     
         if not englishContextoDone:
             if englishContextoSimilarity== (-1000):
@@ -190,7 +77,6 @@ def EnglishForm(guessesPath, englishContexto):
             f.close() 
                 
     return 
-
 
 def color(x):
     cold= 0
@@ -286,7 +172,3 @@ def createRussianEnviroment():
 #main code
 
 formCreation()
-
-
-
-
